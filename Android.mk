d85fef454585e067c446937eb2332827e6931306
picasso09 <picasso09DV@proton.me>
2026-03-18 05:58:16 +0700

    camera: fix typo path
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- Android.mk
+++ Android.mk
@@ -8,12 +8,12 @@ ifneq ($(filter rock,$(TARGET_DEVICE)),)
 
 CAMERA_LIBRARIES := libcamera_algoup_jni.xiaomi.so libcamera_mianode_jni.xiaomi.so libcamera_ispinterface_jni.xiaomi.so libmtkisp_metadata_sys.so vendor.mediatek.hardware.camera.isphal@1.0.so vendor.mediatek.hardware.camera.isphal-V1-ndk.so
 
-CAMERA_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/MiuiCamera/lib/arm64/,$(notdir $(CAMERA_LIBRARIES)))
+CAMERA_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/MiuiCamera/lib/arm64/,$(notdir $(CAMERA_LIBRARIES)))
 $(CAMERA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "MiuiCamera lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(CAMERA_SYMLINKS)
 
