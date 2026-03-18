bd0f9662d990a50e55158d5a41994ac0e5c6f7db
picasso09 <picasso09DV@proton.me>
2026-03-18 15:43:05 +0700

    camera: copy graphics common-V4 from vndk
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -61,10 +61,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libgui_shim_miuicamera.so" "${2}"
             ;;
-        system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V4-ndk.so" "android.hardware.graphics.common-V7-ndk.so" "${2}"
-            ;;
     esac
 }
 
