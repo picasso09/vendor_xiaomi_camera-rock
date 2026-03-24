4913454fcf09b49704c9152e54c687c9c7ff1e80
picasso09 <picasso09DV@proton.me>
2026-03-24 13:30:56 +0000

    camera: drop libgui-xiaomi
    Signed-off-by: picasso09 <picasso09DV@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -60,10 +60,8 @@ function blob_fixup() {
         system_ext/lib64/libcamera_ispinterface_jni.xiaomi.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libgui_shim_miuicamera.so" "${2}"
-            "${PATCHELF}" --replace-needed "libgui.so" "libgui-xiaomi.so" "${2}"
             ;;
-        system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so|\
-        system_ext/lib64/libgui-xiaomi.so)
+        system_ext/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V4-ndk.so" "android.hardware.graphics.common-V7-ndk.so" "${2}"
             ;;
